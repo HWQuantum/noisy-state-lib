da74dab60f84ae6a4c2d5831fa58fe09e756992c
Max Tyler <maxastyler@gmail.com>
2020-03-30 17:04:03 +0100

    Add install requirement

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+install_requires = ["numpy>=1.15"]
+    
 setuptools.setup(
     name="noisy-state-lib-maxastyler", # Replace with your own username
     version="0.0.1",
@@ -13,6 +15,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/HWQuantum/noisy-state-lib",
     packages=setuptools.find_packages(),
+    install_requires=install_requires,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
