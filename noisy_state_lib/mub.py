609881938072b7c3a9a85f0f2620b6f935875f13
Max Tyler <maxastyler@gmail.com>
2020-03-30 15:42:35 +0100

    Add doc strings

--- noisy_state_lib/mub.py
+++ noisy_state_lib/mub.py
@@ -1,14 +1,17 @@
+"""Contains functions to do with MUBs, like generating bases and taking measurements
+"""
+
 import numpy as np
 
 
 def basis(dim, a, n):
-    '''Generate the basis vectors of the mutually unbiased bases in dim = 2j+1
+    """Generate the basis vectors of the mutually unbiased bases in dim = 2j+1
     dimensions
     The index a ∈ (0, 2j+1) (dim+1 bases) denotes which MUB the vector is drawn from
     a=0 gives the computational basis
     The index n ∈ (0, 2j) denotes which vector is chosen
     Taken from the paper: https://arxiv.org/pdf/quant-ph/0601092.pdf
-    '''
+    """
     if a == 0:
         v = np.zeros(dim, dtype=np.complex128)
         v[n % dim] = 1
