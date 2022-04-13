7d6ae65d4fbd45841d98f05c1b537b57e94f4bd0
Max Tyler <maxastyler@gmail.com>
2022-04-13 13:40:21 +0100

    Delete some files

--- shell.nix
+++
@@ -1,9 +0,0 @@
-let pkgs = import <nixpkgs> { };
-in with pkgs;
-mkShell {
-  buildInputs = [
-    nixfmt
-    (python3.withPackages
-      (ps: with ps; [ numpy setuptools yapf matplotlib numba ]))
-  ];
-}
