94f2f9f8eacea6f051e9bd87db01ba5c57814ef9
Max Tyler <maxastyler@gmail.com>
2022-04-13 13:08:17 +0100

    Add flake for noisy-state-lib

--- flake.nix
+++ flake.nix
@@ -11,7 +11,17 @@
       let
         pkgs = import nixpkgs { inherit system; };
 
-      in {
-        defaultPackage = import ./default.nix;
+      in
+      {
+        defaultPackage = pkgs.python3Packages.buildPythonPackage rec {
+          name = "noisy-state-lib";
+          version = "0.0.1";
+          src = ./.;
+          propagatedBuildInputs = with pkgs.python3Packages; [
+            numpy
+            matplotlib
+            numba
+          ];
+        };
       });
 }
